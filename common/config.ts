54f494a125db8ac81d386a3986406f337dc6b7e9
YOGESHWARAN R <yogeshin247@gmail.com>
2024-10-02 15:56:47 +0530

    Update config.ts

--- common/config.ts
+++ common/config.ts
@@ -1,5 +1,5 @@
 export class Config
 {
     public static SERVER_URL: string = "http://localhost:3000";
-    public static CLIENT_URL: string = "http://localhost:4200";
+    public static CLIENT_URL: string = "https://yogeshwaran01.github.io/";
 }
